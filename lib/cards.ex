e537aa520b7c745cc50ddeec8a7ea3b421f04e0c
Mark Zsombor <markszsombor@gmail.com>
2018-09-01 22:05:30 -0700

    deck is now generated with suits and vaules

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,11 +1,30 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three", "Four"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Jack", "Queen", "King"]
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+    # List comprehension, its a map function
+    # Nesting comprehensions leads to an list of lists, needs to be flattened
+    # cards = for suit <- suits do
+    #   for value <- values do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    # This will create a single list, already flattened
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(hand, card) do
+    Enum.member?(hand, card)
   end
 
 end
