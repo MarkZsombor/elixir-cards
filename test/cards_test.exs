3b09485373f85ebbe76a00f1f8326a9a26c2c34a
Mark Zsombor <markszsombor@gmail.com>
2018-09-15 20:23:53 -0700

    wrote some unit tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,6 +2,15 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck makes 32 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 32
+  end
   
-  
+  test "shuffle changes the order of a deck" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
 end
+
+# Assert needs to be true to pass, refute needs false to pass
